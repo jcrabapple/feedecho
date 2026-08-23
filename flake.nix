ce45ec05086c1974101f471aa8150c8a6e2ba885
Jason Crabtree <jason.crabtree@gmail.com>
2026-08-22 21:47:34 -0400

    Bump version to 1.12.0 (multi-tenant release)

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         in
         python.pkgs.buildPythonApplication {
           pname = "feedecho";
-          version = "1.11.1";
+          version = "1.12.0";
           src = ./.;
           format = "pyproject";
 
--- nix/package.nix
+++ nix/package.nix
@@ -9,12 +9,12 @@
 
 pythonPkg.pkgs.buildPythonApplication {
   pname = "feedecho";
-  version = "1.11.1";
+  version = "1.12.0";
 
   src = if src != null then src else fetchFromGitHub {
     owner = "jcrabapple";
     repo = "feedecho";
-    rev = "v1.11.1";
+    rev = "v1.12.0";
     hash = lib.fakeHash; # replace after first build: `nix-prefetch-url --unpack <url>`
   };
 

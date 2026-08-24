9b1f9dca8a22e584bc7bc53498f2f539ac6067b6
Jason Crabtree <jason.crabtree@gmail.com>
2026-08-24 03:32:07 -0400

    Bump version to 1.13.2 (timezone + drip status release)

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         in
         python.pkgs.buildPythonApplication {
           pname = "feedecho";
-          version = "1.13.1";
+          version = "1.13.2";
           src = ./.;
           format = "pyproject";
 

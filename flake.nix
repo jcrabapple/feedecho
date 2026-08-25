d48c170fd162cf83a4df48e22525190e121a00c2
Jason Crabtree <jason.crabtree@gmail.com>
2026-08-25 09:20:39 -0400

    Bump version to 1.13.5 (Postgres dialect + review fix release)

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         in
         python.pkgs.buildPythonApplication {
           pname = "feedecho";
-          version = "1.13.4";
+          version = "1.13.5";
           src = ./.;
           format = "pyproject";
 

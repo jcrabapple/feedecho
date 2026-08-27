648003a88fe3229a456ce3e337d6d504d4ebbc38
Jason Crabtree <jason.crabtree@gmail.com>
2026-08-27 15:32:22 -0400

    Bump version to 1.17.0 (SSRF pinning release)

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         in
         python.pkgs.buildPythonApplication {
           pname = "feedecho";
-          version = "1.16.0";
+          version = "1.17.0";
           src = ./.;
           format = "pyproject";
 

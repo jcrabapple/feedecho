66a3d435a070a15ee565cf87466a40836713e1fa
Jason Crabtree <jason.crabtree@gmail.com>
2026-08-28 02:40:14 -0400

    Bump version to 1.24.2 (Mastodon post links)

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         in
         python.pkgs.buildPythonApplication {
           pname = "feedecho";
-          version = "1.24.1";
+          version = "1.24.2";
           src = ./.;
           format = "pyproject";
 

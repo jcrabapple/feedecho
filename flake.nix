cb389caf5d9827983633de05c0f59589f7d37c24
Jason Crabtree <jason.crabtree@gmail.com>
2026-08-27 20:34:25 -0400

    Bump version to 1.23.0 (tagline release)

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         in
         python.pkgs.buildPythonApplication {
           pname = "feedecho";
-          version = "1.22.0";
+          version = "1.23.0";
           src = ./.;
           format = "pyproject";
 

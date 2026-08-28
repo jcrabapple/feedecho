f1adb31946ad7c6b6d0b4212bf3e53025d83598b
Jason Crabtree <jason.crabtree@gmail.com>
2026-08-27 22:00:04 -0400

    Bump version to 1.24.1 (mobile landing fix)

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         in
         python.pkgs.buildPythonApplication {
           pname = "feedecho";
-          version = "1.24.0";
+          version = "1.24.1";
           src = ./.;
           format = "pyproject";
 

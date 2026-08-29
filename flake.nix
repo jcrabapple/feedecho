60c3f4b15fc4619d2e07f8547d02a613685f4730
Jason Crabtree <jason.crabtree@gmail.com>
2026-08-29 03:16:56 -0400

    chore: bump version to 1.30.0

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         in
         python.pkgs.buildPythonApplication {
           pname = "feedecho";
-          version = "1.29.0";
+          version = "1.30.0";
           src = ./.;
           format = "pyproject";
 
